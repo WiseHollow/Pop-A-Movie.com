d8132ec3c528a6ca8f1c357d34f797d43527a617
WiseHollow <ieatlotz@gmail.com>
2017-12-06 14:48:57 -0500

    Created a better style of search results (displays genre and tags).

--- search.php
+++ search.php
@@ -9,7 +9,7 @@
     <link href="/css/jscrollpane.css" rel="stylesheet">
     <link href="/css/navbar.css" rel="stylesheet">
     <link href="/css/base.css" rel="stylesheet">
-    <link href="/css/pages/video.css" rel="stylesheet">
+    <link href="/css/pages/search.css" rel="stylesheet">
     <link href="/css/font-awesome.min.css" rel="stylesheet">
 
     <script src="/js/popper.js"></script>
@@ -45,19 +45,48 @@
       <!-- Tab panes -->
       <div class="tab-content card-block" align="center">
         <div class="tab-pane active">
-          
+
           <h3>Results for: QUERY HERE</h3>
           <figure class="figure">
-            <img src="http://via.placeholder.com/100x100" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-            <figcaption class="figure-caption">Title</figcaption>
+            <a href="#"><img src="http://via.placeholder.com/150x150" class="figure-img img-fluid rounded" alt="Movie Title 1"></a>
+            <figcaption class="figure-caption title"><a href="#">Movie Title 1</a></figcaption>
+            <figcaption class="figure-caption">
+              <a href="#">Drama</a>,
+              <a href="#">Romance</a>,
+              <a href="#">Asian</a>
+            </figcaption>
+            <figcaption class="figure-caption">
+              <a href="#">korean drama</a>,
+              <a href="#">k-drama</a>
+            </figcaption>
           </figure>
+
           <figure class="figure">
-            <img src="http://via.placeholder.com/100x100" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-            <figcaption class="figure-caption">Title</figcaption>
+            <a href="#"><img src="http://via.placeholder.com/150x150" class="figure-img img-fluid rounded" alt="Movie Title 2"></a>
+            <figcaption class="figure-caption title"><a href="#">Movie Title 2</a></figcaption>
+            <figcaption class="figure-caption">
+              <a href="#">Drama</a>,
+              <a href="#">Romance</a>,
+              <a href="#">Asian</a>
+            </figcaption>
+            <figcaption class="figure-caption">
+              <a href="#">korean drama</a>,
+              <a href="#">k-drama</a>
+            </figcaption>
           </figure>
+
           <figure class="figure">
-            <img src="http://via.placeholder.com/100x100" class="figure-img img-fluid rounded" alt="A generic square placeholder image with rounded corners in a figure.">
-            <figcaption class="figure-caption">Title</figcaption>
+            <a href="#"><img src="http://via.placeholder.com/150x150" class="figure-img img-fluid rounded" alt="Movie Title 3"></a>
+            <figcaption class="figure-caption title"><a href="#">Movie Title 3</a></figcaption>
+            <figcaption class="figure-caption">
+              <a href="#">Drama</a>,
+              <a href="#">Romance</a>,
+              <a href="#">Asian</a>
+            </figcaption>
+            <figcaption class="figure-caption">
+              <a href="#">korean drama</a>,
+              <a href="#">k-drama</a>
+            </figcaption>
           </figure>
 
         </div>
