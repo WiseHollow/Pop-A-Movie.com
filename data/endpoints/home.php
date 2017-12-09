5e740af08521412d3f9df8f1d68aa8fb04becf08
WiseHollow <ieatlotz@gmail.com>
2017-12-08 22:25:03 -0500

    Random movies will never be adult movies.

--- data/endpoints/home.php
+++ data/endpoints/home.php
@@ -38,11 +38,16 @@ function getLatestMovies($conn) {
 }
 
 function getRandomMovies($conn) {
-  $sql = 'SELECT movies.id, title, thumbnail
-  FROM movies
-  JOIN movies_thumbnails ON movies_thumbnails.id = movies.id
-  ORDER BY RAND()
-  LIMIT 16';
+  // Select random movies that are not ADULT movies.
+  $sql = 'SELECT * FROM movies
+    JOIN movies_thumbnails ON movies_thumbnails.id = movies.id
+    WHERE movies.id NOT IN
+    (
+        SELECT movies_genres.movie_id FROM movies_genres
+    	WHERE movies_genres.genre_id = 22
+    )
+    ORDER BY RAND()
+    LIMIT 16';
 
   $result = $conn->query($sql);
   $movies = array();
